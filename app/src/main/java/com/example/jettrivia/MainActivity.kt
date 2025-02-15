09914a158ec030e299dae8af128d0a75bc342a58
Matheus Souza <matheus621cardoso@gmail.com>
2025-02-15 16:48:20 -0300

    making the first request with the retrofit

--- app/src/main/java/com/example/jettrivia/MainActivity.kt
+++ app/src/main/java/com/example/jettrivia/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.jettrivia
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.jettrivia.screens.QuestionsViewModel
 import com.example.jettrivia.ui.theme.JetTriviaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +27,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    TriviaHome()
                 }
             }
         }
     }
 }
 
+@Composable
+fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {
+    Questions(viewModel)
+}
+
+@Composable
+fun Questions(viewModel: QuestionsViewModel) {
+
+    val questions = viewModel.data.value.data?.toMutableList()
+    if (viewModel.data.value.loading == true) {
+        Log.d("Carregando", "Questions: ... carregando")
+    } else {
+        Log.d("Carregando", "Questions: ... parou de carregar")
+        questions?.forEach { questionItem ->
+            Log.d("Resultado", "Questions: ${questionItem.question}")
+        }
+    }
+
+
+}
 
 @Preview(showBackground = true)
 @Composable
