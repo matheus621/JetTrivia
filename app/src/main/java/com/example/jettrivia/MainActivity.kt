e023cd69852300a32a16a100407e7878fec0cb73
Matheus Souza <matheus621cardoso@gmail.com>
2025-02-15 18:06:58 -0300

    displaying the first question on the screen

--- app/src/main/java/com/example/jettrivia/MainActivity.kt
+++ app/src/main/java/com/example/jettrivia/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.jettrivia
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +9,7 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.jettrivia.screens.QuestionsViewModel
+import com.example.jettrivia.screens.TriviaHome
 import com.example.jettrivia.ui.theme.JetTriviaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,27 +30,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {
-    Questions(viewModel)
-}
-
-@Composable
-fun Questions(viewModel: QuestionsViewModel) {
-
-    val questions = viewModel.data.value.data?.toMutableList()
-    if (viewModel.data.value.loading == true) {
-        Log.d("Carregando", "Questions: ... carregando")
-    } else {
-        Log.d("Carregando", "Questions: ... parou de carregar")
-        questions?.forEach { questionItem ->
-            Log.d("Resultado", "Questions: ${questionItem.question}")
-        }
-    }
-
-
 }
 
 @Preview(showBackground = true)
